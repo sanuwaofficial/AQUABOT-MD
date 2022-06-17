af1f77f7d8e2aa77e327bf59d1d17a4477e83254
Pathum20 <107669326+Pathum20@users.noreply.github.com>
2022-06-17 11:54:29 +0530

    Create play.js

---
+++ plugins/play.js
@@ -0,0 +1,37 @@
+const { servers, yta, ytv } = require('../lib/y2mate')
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} PaniPalli`
+  let chat = global.db.data.chats[m.chat]
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw 'Content Not found'
+  let isVideo = /2$/.test(command)
+  let yt = false
+  let yt2 = false
+  let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry again...'}`)
+    }
+  }
+  let { dl_link, thumb, title, filesize, filesizeF } = yt
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*🔮 Title:* ${title}
+*🔻 Audio File Size:* ${filesizeF}
+*🔻 Video File Size:* ${yt2.filesizeF}
+*🍃 Server:* ${usedServer}
+`.trim(), '© Lakiya 🤍🥀', 'Audio', `.yta ${vid.url}`, 'Video', `.ytv ${vid.url}`)
+}
+handler.help = ['song','play','?','yt'].map(v => v + ' <query>')
+handler.tags = ['downloader']
+handler.command = /^(play|yt|song)$/i
+
+module.exports = handler
